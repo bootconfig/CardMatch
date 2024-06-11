c2fdc5806b1c65610115e916278610fabf5e3dd3
bootconfig <online1@kevinfordemail.com>
2024-06-10 20:43:05 -0500

    Updated App

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+
+    static int add(int x, int y) {
+        return x + y;
+    }
+
+    static void print() {
+        System.out.print("hi");
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("I Love Jesus!");
+        System.out.println("programming is fun");
+        System.out.println(add(1, 1));
+    }
+}
