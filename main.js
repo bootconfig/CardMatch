e124a95210629ab4abb19b206b97e559c232ae1f
bootconfig <online1@kevinfordemail.com>
2024-07-08 21:15:54 -0500

    card layout displayed

--- main.js
+++ main.js
@@ -4,21 +4,42 @@ let triangle = "triangle.png";
 let pentagone = "pentagone.png";
 let free = "free.png";
 
-function Card(image, description, visible, id) {
-    this.image = image;
-    this.description = description;
-    this.visible = visible;
-    this.id = id;
+class Card {
+    constructor(image, description, status) {
+        this.image = image;
+        this.description = description;
+        this.status = status;
+        this.card = this.#createButton();
+    }
+
+    #createButton() { 
+        const btn = document.createElement('button');
+        btn.style.background=`url(${this.image})`;
+        btn.style.width = "256px";
+        btn.style.height = "256px";
+        
+        return btn;
+    }
 }
 
-let images = [free, square, square, circle, circle, triangle, triangle, pentagone, pentagone];
-let cards = [];
 
-for (let i=0; i<9; i++) {
-        const card = new Card(images[i], "Card name: " + images[i], true, i);
-        cards.push(card);
-    }
+const Board = {
+    cards:[],
+    create: function() {
 
-for (let i=0; i<9; i++) {
-        document.getElementById("descriptions").innerHTML +=cards[i].description + "<br>";
+        for(let i=0; i<2; i++) {
+            Board.cards.push(new Card(square, "square", "DOWN"));
+            Board.cards.push(new Card(circle, "circle", "DOWN"));
+            Board.cards.push(new Card(triangle, "triangle", "DOWN"));
+            Board.cards.push(new Card(pentagone, "pentagone", "DOWN"));
+        }
+        
+        Board.cards.push(new Card(free, "free", "UP"));
+
+        for(let i=0; i<9; i++) {
+            document.body.appendChild(Board.cards[i].card);
+        }
     }
+}
+
+Board.create();
